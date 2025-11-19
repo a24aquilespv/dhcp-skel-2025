40b9e81610023f486852309de266dc2d66d382be
a24aquilespv <a24aquilespv@iessanclemente.net>
2025-11-19 10:27:22 +0100

    Arreglar ddns

--- ned/entrypoint.sh
+++ ned/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
 
-rm -f /var/run/kea/kea-dhcp4.kea-dhcp4.pid
+rm -f /var/run/kea/kea-dhcp4.kea-dhcp4.pid /run/kea/kea-dhcp-ddns.kea-dhcp-ddns.pid
+                                           
+                                        
 ip route delete default
 ip route add default via 192.168.10.5
-kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d
+
+kea-dhcp-ddns -c /etc/kea/kea-dhcp-ddns.conf -d &
+kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d 
