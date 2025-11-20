d029d14804a6c1ea65a5d3c97e5d5cf7eec1a8f1
a24aquilespv <a24aquilespv@iessanclemente.net>
2025-11-20 12:54:34 +0100

    Configur log kea en ned - kea-dhcp4 - kea-dhcp-ddns - Como la versión kea empleada es la 2.2, utilizar output_options en vez de output-options - Almacenar todos los logs en un named volume de docker - Configurar los permisos para que el usuario _kea sea grupo propietario de/var/log/kea - Configurar permisos para que el grupo _kea tenga permisos de escritura (770)

--- ned/entrypoint.sh
+++ ned/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 
 
-rm -f /var/run/kea/kea-dhcp4.kea-dhcp4.pid /run/kea/kea-dhcp-ddns.kea-dhcp-ddns.pid
+/usr/bin/rm -f /var/run/kea/kea-dhcp4.kea-dhcp4.pid /run/kea/kea-dhcp-ddns.kea-dhcp-ddns.pid
                                            
                                         
-ip route delete default
-ip route add default via 192.168.10.5
+/usr/sbin/ip route delete default
+/usr/sbin/ip route add default via 192.168.10.5
 
+# Crear carpeta para los logs
+log_path="/var/log/kea/"
+
+/usr/bin/mkdir -p "${log_path}"
+[[ -d "${log_path}" ]] && /usr/bin/chown root:_kea "${log_path}" && /usr/bin/chmod 770 "${log_path}"
+
+# Levantar servidor ddns y dhcp4
 kea-dhcp-ddns -c /etc/kea/kea-dhcp-ddns.conf -d &
-kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d 
+kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d
