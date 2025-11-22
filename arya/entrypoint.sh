fc529cc080a522e8db3ef762234c7426870d834b
AKILES <akiles@debianBSPWM>
2025-11-22 16:59:48 +0100

    Muchos cambios - Hacer que las interfaces del router siempre tengan el mismo nombre simbólico - Hacer que se actualicen las zonas dns dinámicamente aunque el DHCID no coincida. Debe de seguir existiendo un DHCID en el RR de la zona - Arreglar cliente lanisger. Le estaba haciendo un flush a una interfaz que no corresponde. Además, estaba eliminando la regla regenal después de que el servidor dhcp le asigne su configuración

--- arya/entrypoint.sh
+++ arya/entrypoint.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 /usr/sbin/ip route delete default
 /usr/sbin/ip route add default via 192.168.10.5
-
-named -u bind "$@"
--- router/entrypoint.sh
+++ router/entrypoint.sh
@@ -1,6 +1,42 @@
 #!/usr/bin/bash
 
 
+# Hacer que cada red de docker siempre tenga el mismo nombre simbólico dentro del contendor 
+
+### BEGIN ###
+
+# Bajar todas las interfaces
+ip link set dev eth0 down
+ip link set dev eth1 down
+ip link set dev eth2 down
+ip link set dev eth3 down
+
+# Asignar un nombre simbólico temporal a cada interfaz
+ip link set eth0 name tmp0
+ip link set eth1 name tmp1
+ip link set eth2 name tmp2
+ip link set eth3 name tmp3
+
+# Obtener el nombre simbólico asignado a cada interfaz
+dev_eth0=$(ip link show | grep -B 1 '00:1a:2b:3c:4d:5e' | head -n 1 | awk '{print $2}' | awk '{print $1}' FS='@')
+dev_eth1=$(ip link show | grep -B 1 'a4:b5:c6:d7:e8:f9' | head -n 1 | awk '{print $2}' | awk '{print $1}' FS='@')
+dev_eth2=$(ip link show | grep -B 1 'de:ad:be:ef:00:01' | head -n 1 | awk '{print $2}' | awk '{print $1}' FS='@')
+dev_eth3=$(ip link show | grep -B 1 '80:5e:c0:93:f6:d0' | head -n 1 | awk '{print $2}' | awk '{print $1}' FS='@')
+
+# Asignarle el nombre simbólico que le corresponde a cada interfaz
+ip link set "${dev_eth0}" name eth0
+ip link set "${dev_eth1}" name eth1
+ip link set "${dev_eth2}" name eth2
+ip link set "${dev_eth3}" name eth3
+
+
+ip link set dev eth0 up
+ip link set dev eth1 up 
+ip link set dev eth2 up 
+ip link set dev eth3 up 
+
+### END ###
+
 # Habilitar enrutamiento entre interfaces
 /usr/bin/echo "1" > /proc/sys/net/ipv4/ip_forward
 
@@ -17,6 +53,5 @@
 
 
 # Servidor relay DHCP
-# Argumento -n para modo no demonio
-dhcp-helper -s 192.168.10.254 -i eth2 -i eth3 -d
-# dhcp-helper -n -s 192.168.10.254 -i eth2
+dhcp-helper -s 192.168.10.254 -i eth0 -i eth1 -d
+
